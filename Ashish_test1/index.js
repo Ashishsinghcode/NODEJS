301aa75af6f02134633f2984c418adb34b91b406
Ashish kumar singh <Ashishsingh.code@gmail.com>
2022-12-02 21:46:02 +0530

    Delete Ashish_test1 directory

--- Ashish_test1/index.js
+++
@@ -1,7 +0,0 @@
-const express =require('express')
-const ser =express()
-ser.use(express.urlencoded({extended:true}))
-const user = require('./routes/user')
-
-ser.use('/userapi',user)
-ser.listen(2000)
